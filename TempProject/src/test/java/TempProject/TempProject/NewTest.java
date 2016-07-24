6121443f0ae7243754054e2d070a65fcae233b94
maram <kayedkmaram@gmail.com>
2016-07-24 17:29:21 +0300

    test3

---
+++ TempProject/src/test/java/TempProject/TempProject/NewTest.java
@@ -0,0 +1,20 @@
+package TempProject.TempProject;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	@Test
+	public void Test1()
+	{
+		Assert.assertEquals(1, 1);
+	}
+	
+	@Test
+	public void Test2()
+	{
+		Assert.assertEquals(1, 1);
+	}
+
+}
